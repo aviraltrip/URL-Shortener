9446a86bbf2de3fb376a488004603ee33435a8f7
aviraltrip <aviraltripathi25@gmail.com>
2026-09-15 12:43:58 +0530

    error handling

--- api/main.go
+++ api/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import {
+	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2"
+}
 
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+	app := fiber.New()
 }
