063e2e56b9ac71e00b0ab68ff4c402dcc79a514a
aviraltrip <aviraltripathi25@gmail.com>
2026-09-15 12:27:14 +0530

    shorten req res

--- api/routers/shorten.go
+++ api/routers/shorten.go
@@ -1,1 +1,15 @@
-package routes
+package routers
+
+type request struct {
+	URL         string `json:"url"`
+	CustomShort string `json:"short"`
+	Expiry      string `json:"expiry"`
+}
+
+type response struct {
+	URL            string `json:"url"`
+	CustomShort    string `json:"short"`
+	Expiry         string `json:"expiry"`
+	XRateRemaining int    `json:"rate_limit"`
+	XRateLimitRest int    `json:"rate_limit_reset"`
+}
