447ad3c6edc3921e58c8bd5eb52e5f33dbc0ae27
aviraltrip <aviraltripathi25@gmail.com>
2026-09-15 15:02:23 +0530

    shorten url func

--- api/routers/shorten.go
+++ api/routers/shorten.go
@@ -13,3 +13,11 @@ type response struct {
 	XRateRemaining int    `json:"rate_limit"`
 	XRateLimitRest int    `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "can't parse json"})
+	}
+}
