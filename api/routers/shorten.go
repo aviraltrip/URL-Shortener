f687bc6d865fcd9396a41fae2a7bd25a563c1bef
aviraltrip <aviraltripathi25@gmail.com>
2026-09-16 00:39:16 +0530

    helpers function added

--- api/routers/shorten.go
+++ api/routers/shorten.go
@@ -1,5 +1,7 @@
 package routers
 
+import "github.com/aviraltrip/urlshortener/helpers"
+
 type request struct {
 	URL         string `json:"url"`
 	CustomShort string `json:"short"`
@@ -20,4 +22,14 @@ func ShortenURL(c *fiber.Ctx) error {
 	if err := c.BodyParser(&body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "can't parse json"})
 	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error", "you cant hack the system"})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
