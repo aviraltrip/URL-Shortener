f74e0297bd0ab8d0b300ce349e0d3a06089e2690
aviraltrip <aviraltripathi25@gmail.com>
2026-09-16 09:28:25 +0530

    resolve func error handling

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -10,4 +10,19 @@ func ResolveURL(c *fiber.Ctx) error {
 
 	r := database.CreateClient(0)
 	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"short not found in the database"})
+	}
+	else if err != nil {
+		return c.Status(fiber.StatusInternalError).JSON(fiber.Map{"error":"cannot connect to DB",
+	})
 }
+
+rInr := database.CreateClient(1)
+defer rInr.Close()
+
+_ = rInr.Incr(database.Ctx, "counter")
+
+return c.Redirect(value, 301)
