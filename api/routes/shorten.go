6c7ff328a442bceae60c75b463fd62f3af001581
aviraltrip <aviraltripathi25@gmail.com>
2026-09-18 11:09:52 +0530

    rate limit reset logic

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -83,6 +83,22 @@ func ShortenURL(c *fiber.Ctx) error {
 			"error" : "Unable to connect to server"
 		})
 	}
-	
+
+	resp := response {
+		URL: body.URL,
+		CustomShort: "",
+		Expiry: body.Expiry,
+		XRateRemaining: 10,
+		XRateLimitReset: 30
+	}
+
 	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.RateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+	
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
 }
